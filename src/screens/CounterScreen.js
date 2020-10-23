de714c9f223795f3d3132c152b856da0768de2bd
Fatma <fceyda@gmail.com>
2020-10-23 13:01:58 -0700

    create basic counter

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
 function CounterScreen() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View>
-      <Text>Current Count:</Text>
+      <Text>Current Count: {counter}</Text>
+      <Button title='Increase' onPress={() => setCounter(counter + 1)}/>
+      <Button title='Decrease' onPress={() => setCounter(counter - 1 )}/>
     </View>
   )
 }
