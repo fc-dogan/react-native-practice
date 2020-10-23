ccb410a37613914cfae3e8cd738f4a20098df565
Fatma <fceyda@gmail.com>
2020-10-23 11:54:41 -0700

    add image screen navigation to home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,6 +15,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={ () => navigation.navigate('List') } 
         title='Go to list demo'
       />
+      <Button 
+        onPress={ () => navigation.navigate('Image') } 
+        title='Go to image demo'
+      />
     </View>
   ) 
 };
